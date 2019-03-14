939a7d5fc965e9e9dc6dbf0ceb6a116ff1b27636
SoYoung210 <ethdud1@gmail.com>
2019-03-14 20:32:44 +0900

    Add default setting

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+	.BundleAnalyzerPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const join = require('path').join;
+
+module.exports = {
+	entry: './src/index',
+	module: {
+		rules: [
+			{
+				test: /\.(ts|tsx|js)?$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+					},
+				],
+			},
+			{
+				test: /\.(html)$/,
+				use: {
+					loader: 'html-loader',
+				},
+			},
+			{
+				test: /\.p?css$/,
+				use: [
+					'style-loader',
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1,
+							modules: true,
+							localIdentName: '[path]_[local]',
+							sourceMap: true,
+						},
+					},
+					'postcss-loader',
+				],
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg|jpg)$/i,
+				use: ['file-loader'],
+			},
+			{
+				test: /\.less$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					{
+						loader: 'less-loader',
+						options: {
+							javascriptEnabled: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.svg$/,
+				use: {
+					loader: 'url-loader',
+					options: {
+						name: '[name].[ext]?[hash]',
+						publicPath: './dist/',
+						limit: 20000, // 20kb
+					},
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: './index.html',
+		}),
+		new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /ko/),
+		new BundleAnalyzerPlugin(),
+	],
+	devServer: {
+		contentBase: join(__dirname, 'dist'),
+		compress: true,
+		port: 3000,
+		historyApiFallback: true,
+	},
+	resolve: {
+		alias: {
+			domain: join(__dirname, 'src/domain/'),
+			presentation: join(__dirname, 'src/presentation/'),
+			utils: join(__dirname, 'src/utils/'),
+			application: join(__dirname, 'src/'),
+		},
+		modules: ['node_modules'],
+		extensions: ['.ts', '.tsx', '.js', '.json', '.less'],
+	},
+};
