ad17e0b080dde88d215099a1984deb085e782cbd
SoYoung210 <ethdud1@gmail.com>
2019-03-18 22:50:27 +0900

    Fix temp data

--- src/api/music/curation/index.ts
+++ src/api/music/curation/index.ts
@@ -6,6 +6,8 @@ import { CurationMusicMapper } from './mapper';
 export class MusicCuratingListApiProvider {
 	get = (): Promise<List<Music>> =>
 		requestGET('seba-choice').then((result: any) =>
-			List(result.map((res: any) => new CurationMusicMapper().fromJson(res))),
+			List.of(
+				result.data.map((res: any) => new CurationMusicMapper().fromJson(res)),
+			),
 		);
 }
