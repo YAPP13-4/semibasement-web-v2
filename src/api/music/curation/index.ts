93f5ec0693caec34f118cc843241f3595bda4d6a
SoYoung210 <ethdud1@gmail.com>
2019-03-19 00:48:16 +0900

    Fix type bug

--- src/api/music/curation/index.ts
+++ src/api/music/curation/index.ts
@@ -5,9 +5,11 @@ import { CurationMusicMapper } from './mapper';
 
 export class MusicCuratingListApiProvider {
 	get = (): Promise<List<Music>> =>
-		requestGET('seba-choice').then((result: any) =>
-			List.of(
-				result.data.map((res: any) => new CurationMusicMapper().fromJson(res)),
-			),
-		);
+		requestGET('seba-choice')
+			.then(res => res.data)
+			.then((data: Array<any>) =>
+				List(
+					data.map((music: any) => new CurationMusicMapper().fromJson(music)),
+				),
+			);
 }
