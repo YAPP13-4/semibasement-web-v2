15835ec411b54c919a1c3c072553b45e0fb16eb1
SoYoung210 <ethdud1@gmail.com>
2019-04-09 21:37:55 +0900

    Change private to public

--- src/domain/repositories/index.ts
+++ src/domain/repositories/index.ts
@@ -1,11 +1,10 @@
-import { MusicCuratingListApiProvider } from 'application/api/music/curation';
 import { CurationMusicRepository } from './music/curation';
 
 export class RepositoryDependencies {
   private curationMusic: CurationMusicRepository;
 
-  public constructor() {
-    this.curationMusic = new CurationMusicRepository(new MusicCuratingListApiProvider());
+  public constructor(curationMusic: CurationMusicRepository) {
+    this.curationMusic = curationMusic;
   }
 
   public getCurationMusic = () => this.curationMusic;
