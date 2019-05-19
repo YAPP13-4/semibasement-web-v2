32fcf6c94c230880d593d20d1ec98f82000fd551
SoYoung210 <ethdud1@gmail.com>
2019-05-19 19:03:42 +0900

    Add util functions

---
+++ src/presentation/redux/actionTypes.ts
@@ -0,0 +1,16 @@
+import { StatusCode } from "application/utils/api";
+import { FetchStatus } from "./FetchStatus";
+
+export const actionTypes = {
+  HOME_MUSIC : 'HOME_MUSIC'
+}
+
+export type ErrorInfo = {
+  statusCode: StatusCode;
+  message?: string;
+};
+
+export type FetchResult = {
+  fetchState: FetchStatus;
+  error?: ErrorInfo;
+};
