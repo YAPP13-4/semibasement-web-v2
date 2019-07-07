1dcdb0aaeb2137d765d025af74a97ab36395ced2
seb <seb@woowahan.com>
2019-07-07 18:54:59 +0900

    [#22] repositoryDependencies 이름 변경

--- src/domain/repositories/index.ts
+++ src/domain/repositories/index.ts
@@ -1,7 +1,9 @@
 import { MusicCuratingListApiProvider } from 'application/api/music';
 import { MusicCurationRepository } from 'application/domain/repositories/music';
+import { UserMeRepository } from './user/userMeRepository';
+import { UserMeProvider } from 'application/api/user';
 
-export class RepositoryDependencies {
+export class MusicRepositoryDependencies {
   private curationMusic: MusicCurationRepository;
 
   public constructor() {
@@ -9,4 +11,14 @@ export class RepositoryDependencies {
   }
 
   public getCurationMusic = () => this.curationMusic;
+}
+
+export class UserRepositoryDependencies {
+  private userMe : UserMeRepository;
+
+  public constructor() {
+    this.userMe = new UserMeRepository(new UserMeProvider());
+  }
+
+  public getUserMe = () => this.userMe;
 }
