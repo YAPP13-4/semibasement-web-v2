4c4a6bc989d53db72afb0762635e53bf335dffc3
seb <seb@woowahan.com>
2019-07-21 20:21:34 +0900

    [#22] UserMeContainer userSelector 적용

--- src/presentation/redux/reducers.ts
+++ src/presentation/redux/reducers.ts
@@ -2,15 +2,18 @@ import { combineReducers } from 'redux';
 import { homeMusicReducer, SebaMusicState } from './getMusicInfo';
 import { loadingReducer, loadingState } from 'application/modules/loading';
 import { playerReducer, PlayerState } from './player';
+import { UserMeState, userMeReducer } from './userMe';
 
 export interface StoreState {
   sebaMusic: SebaMusicState;
   player: PlayerState;
   loading: loadingState;
+  userMe: UserMeState;
 }
 
 export default combineReducers({ 
   sebaMusic: homeMusicReducer,
   loading: loadingReducer,
   player: playerReducer,
+  userMe: userMeReducer,
 });
